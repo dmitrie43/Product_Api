afef4eec8e9c03bab4709e96883b227fd8be6533
dmitrie43 <40702400+dmitrie43@users.noreply.github.com>
2018-12-13 16:31:25 +0400

    Initial commit

---
+++ app/Http/Controllers/API/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController as BaseController;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class RegisterController extends Controller
+{
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return $this->sendError('Validation Error', $validator->errors());
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+        $success['token'] = $user->createToken('MyApp')->accessToken;
+        $success['name'] = $user->name;
+
+        return $this->sendResponse($success, 'User register successfully');
+    }
+}
